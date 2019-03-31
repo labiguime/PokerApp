741633ab5b494afa11f069099300861903cc3a0a
labiguime <leoabiguime@gmail.com>
2019-03-31 14:30:45 -0700

    Fixed home page join button

--- app/src/main/java/com/example/lepti/pokerapp/HomePage.java
+++ app/src/main/java/com/example/lepti/pokerapp/HomePage.java
@@ -1,8 +1,11 @@
 package com.example.lepti.pokerapp;
 
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -12,6 +15,7 @@ import android.widget.ImageView;
 public class HomePage extends AppCompatActivity {
     ImageView app_logo;
     Animation fromtop;
+    CardView join_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +23,14 @@ public class HomePage extends AppCompatActivity {
         app_logo = (ImageView) findViewById(R.id.hp_app_logo);
         fromtop = AnimationUtils.loadAnimation(this, R.anim.fromtop);
         app_logo.setAnimation(fromtop);
+        join_button = findViewById(R.id.cardView);
+
+        join_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomePage.this, GamePage.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
