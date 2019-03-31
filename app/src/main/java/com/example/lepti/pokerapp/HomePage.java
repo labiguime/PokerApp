0449ddbb3736bfbe4d4b768ebdc5b359fe48f191
labiguime <leoabiguime@gmail.com>
2019-03-30 17:24:59 -0700

    Home page UI complete

--- app/src/main/java/com/example/lepti/pokerapp/HomePage.java
+++ app/src/main/java/com/example/lepti/pokerapp/HomePage.java
@@ -1,13 +1,23 @@
 package com.example.lepti.pokerapp;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class HomePage extends AppCompatActivity {
-
+    ImageView app_logo;
+    Animation fromtop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        app_logo = (ImageView) findViewById(R.id.hp_app_logo);
+        fromtop = AnimationUtils.loadAnimation(this, R.anim.fromtop);
+        app_logo.setAnimation(fromtop);
     }
 }
